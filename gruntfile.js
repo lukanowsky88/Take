fe589ca0b040ea0d2d17df77103b84923dc44411
plukanowski <p.lukanowski@wp.pl>
2017-10-14 00:46:23 +0200

    Adding pages

---
+++ gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  	sass: {
+    	options: {
+        	sourceMap: true
+      },
+    	dist: {
+			files: {
+        	'css/main.css': 'sass/main.scss'
+        }
+      }
+    },
+	  
+  	imagemin: {
+  		dynamic: {
+			options: {
+				optimizationLEvel: 1
+			},
+  			files: [{
+  				expand: true,
+  				cwd: 'images',
+  				src: ['**/*.{png,jpg,gif}'],
+  				dest: 'imagesmin/'
+  			}]
+  		}
+  	},
+	watch: {
+    	scripts: {
+			files: ['sass/*.scss'],
+			tasks: ['sass'],
+        options: {
+            spawn: false,
+        },
+    } 
+},
+   browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'app/css/*.css',
+                        'app/*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './app'
+                }
+            }
+        }
+	 
+ });
+  // Load the plugins tasks 
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+	
+  // Default task(s).
+  grunt.registerTask('default', ['sass', 'imagemin', 'browserSync']);
+};
